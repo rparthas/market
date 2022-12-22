1caf594fcf952b0433a24d9d73f627c0b3bd819d
psrajagilead <117930023+psrajagilead@users.noreply.github.com>
2022-12-22 16:24:06 +0530

    Raja | Test using a source distribution

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 import sys
 import os
 
-from setuptools import setup
+from setuptools import setup,find_packages
 setup(name="market",
       version="0.3",
       url='https://github.com/srijan-stack/market',
       description="this is a market calculator",
       long_description="use this to market",
       author="srijan",
-      packages=["src"],
+      packages=find_packages(include=['src']),
       install_requires=[])
